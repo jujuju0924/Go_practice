1876ff20fb54688d1f4861d6ac0f8050bcc70de2
jumpei arai <jumpei0924@gmail.com>
2022-06-05 14:20:55 +0900

    QRコード作成完了

--- server/main.go
+++ server/main.go
@@ -55,6 +55,7 @@ func generateQRCode(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	png.Encode(w, img)
 }
 
